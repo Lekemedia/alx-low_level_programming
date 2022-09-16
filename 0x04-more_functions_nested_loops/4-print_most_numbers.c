ab23dbbcdf562e47cfd04b095fb98f70b8453b54
Ogaboss <108877570+Lekemedia@users.noreply.github.com>
2022-09-16 21:50:10 +0100

    error fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * print_most_numbers - prints 0 - 9 except 2 , 3
- * Description: prints
- * Return: void
- */
-void print_most_numbers(void)
-{
-	int a = 0;
-	while (a <= 9)
-	{
-		if (a != 2 && a != 4)
-		{
-			_putchar(a + '0');
-		}
-		a++;
-	}
-	_purchar('\n');
-}
-=======
 #include "main.h"
 
 /**
@@ -42,4 +20,3 @@ void print_most_numbers(void)
 	}
 	_putchar('\n');
 }
->>>>>>> e9189db5ed1541851b6357905350dea9e91e9ac2
