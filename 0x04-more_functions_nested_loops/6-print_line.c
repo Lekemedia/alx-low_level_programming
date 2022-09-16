6d940f839410aae6fc5d056cf522334f574d0c43
Ogaboss <108877570+Lekemedia@users.noreply.github.com>
2022-09-16 21:59:09 +0100

    cdfhytu

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * print_line - prints a line
- * @n: variable
- * Return: 0
- */
-void print_line(int n)
-{
-	int a = 0;
-	while (a < n && n > 0)
-	{
-		_putchar('_');
-		a++;
-	}
-	_putchar('\n');
-}
-=======
 #include "main.h"
 
 /**
@@ -37,4 +18,3 @@ void print_line(int n)
 	}
 	_putchar('\n');
 }
->>>>>>> e9189db5ed1541851b6357905350dea9e91e9ac2
