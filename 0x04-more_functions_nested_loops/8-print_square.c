03ec32b490f0518b369fd0da605d420f1df51cc5
Ogaboss <108877570+Lekemedia@users.noreply.github.com>
2022-09-16 22:01:22 +0100

    err

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * print_square - check for a digit
- * @n: number of _ to be printed
- * Return: void
- */
-void print_square(int n)
-{
-	int i = 0, ii;
-	while (i < n  n > 0)
-	{
-		ii = 0;
-		while (ii < n)
-		{
-			_putchar('#');
-			ii++;
-		}
-		_putchar('\n');
-		i++;
-	}
-	if (i == 0)
-		_putchar('\n');
-}
-=======
 #include "main.h"
 
 /**
@@ -50,4 +24,3 @@ void print_square(int n)
 	if (i == 0)
 		_putchar('\n');
 }
->>>>>>> e9189db5ed1541851b6357905350dea9e91e9ac2
