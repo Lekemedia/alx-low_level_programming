ec58608ba79bcc6d290f9eece1011562b23837db
Ogaboss <108877570+Lekemedia@users.noreply.github.com>
2022-09-16 21:51:47 +0100

    dos ma pami now

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * more_numbers - prints 0 - 14
- * Return: void
- */
-void more_numbers(void)
-{
-	char n, c;
-	int i = 0;
-	while (i < 10)
-	{
-		for (n = 0; n <= 14; n++)
-		{
-			c = n;
-			if (n > 9)
-			{
-				_putchar('1');
-				c = n % 10;
-			}
-			_purchar('0' + c);
-		}
-		_putchar('\n');
-		i++;
-	}
-}
-=======
 #include "main.h"
 
 /**
@@ -56,4 +28,3 @@ void more_numbers(void)
 		i++;
 	}
 }
->>>>>>> e9189db5ed1541851b6357905350dea9e91e9ac2
