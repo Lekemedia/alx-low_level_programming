2d57ab22daf4b9d0ec56dfaa332a4d271205b299
Ogaboss <108877570+Lekemedia@users.noreply.github.com>
2022-09-16 21:44:53 +0100

    fixed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * print_numbers - print 0 - 9
- * Return: void
- */
-void print_numbers(void)
-{
-	char a = 0;
-	while (a <= 9)
-	{
-		_putchar(a + '0');
-		a++;
-	}
-	_putchar('\n');
-}
-=======
 #include "main.h"
 
 /**
@@ -35,4 +17,3 @@ void print_numbers(void)
 	}
 	_putchar('\n');
 }
->>>>>>> e9189db5ed1541851b6357905350dea9e91e9ac2
