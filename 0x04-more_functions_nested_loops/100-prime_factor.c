b09ed0f0b3fe2a060e57e4d21f7e54b9d117e502
Ogaboss <108877570+Lekemedia@users.noreply.github.com>
2022-09-16 22:10:18 +0100

    error fixed

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-/**
- * main - prime numbers
- * Return: 0
- */
-int main(void)
-{
-	long num = 612852475143;
-	long divisor = 2;
-	long larg_prim = 0;
-	while (num != 1)
-	{
-		if (num % divisor == 0)
-		{
-			num = num / divisor;
-			larg_prim = divisor;
-		}
-		divisor += 1;
-	}
-	printf("%ld\n", larg_prim);
-	return (0);
-}
-=======
 #include <stdio.h>
 
 /**
@@ -49,4 +24,3 @@ int main(void)
 	printf("%ld\n", larg_prim);
 	return (0);
 }
->>>>>>> e9189db5ed1541851b6357905350dea9e91e9ac2
