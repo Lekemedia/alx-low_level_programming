446ec72ae1a922280eec8092e2b62c17da28c4b7
Ogaboss <108877570+Lekemedia@users.noreply.github.com>
2022-09-16 22:11:32 +0100

    error fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-/**
- * print_number - prints numbers
- * @n: number to be printed
- * Return:void
- */
-
-void print_number(int n)
-{_
-	unsigned int x;
-	x = n;
-	if (n < 0)
-	{
-		_putchar(45);
-		x = -n;
-	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
-}
-=======
 #include "main.h"
 /**
  * print_number - prints numbers
@@ -41,4 +19,3 @@ void print_number(int n)
 		print_number(x / 10);
 	_putchar((x % 10) + '0');
 }
->>>>>>> e9189db5ed1541851b6357905350dea9e91e9ac2
