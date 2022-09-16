14ee5bbf080bb2e8cfb866bc6899e9acd75a29e9
Ogaboss <108877570+Lekemedia@users.noreply.github.com>
2022-09-16 22:02:42 +0100

    bug fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-/**
- * main - fizz buzz code
- * Return: void
- */
-int main(void)
-{
-	int i = 1;
-	while (i <= 100)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz")
-		}
-		else if (i % 5 --9)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%i", i);
-		}
-		if (i != 100)
-		{
-			putchar(' ');
-		}
-		i++;
-	}
-	putchar('\n');
-	rerturn (0);
-}
-=======
 #include <stdio.h>
 
 /**
@@ -76,4 +38,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
->>>>>>> e9189db5ed1541851b6357905350dea9e91e9ac2
