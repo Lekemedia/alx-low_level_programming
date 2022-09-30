a7f70ba851031d643705688f2ccaef49f59418a0
Akinduyo Emmanuel <108877570+Lekemedia@users.noreply.github.com>
2022-09-30 23:00:29 +0100

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,43 @@
+
+Alouie412
+/
+holbertonschool-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+holbertonschool-low_level_programming/0x0A-argc_argv/1-args.c
+@Alouie412
+Alouie412 Initial commit
+ 1 contributor
+26 lines (21 sloc)  368 Bytes
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
+	return (0);
+}
